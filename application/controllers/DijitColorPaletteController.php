4f896d1ae5c297919719e6f530b3780150baa832
merizamed <merizamed@4b472d0a-6b82-7481-57af-a57a79467d2e>
2010-04-11 10:13:10 +0000

    added code for the Dojo Forms into the Views

--- application/controllers/DijitColorPaletteController.php
+++ application/controllers/DijitColorPaletteController.php
@@ -15,5 +15,8 @@ class DijitColorPaletteController extends Zend_Controller_Action
     public function paletteInFormAction()
     {
         $this->view->form = new Default_Form_ColorPaletteExample();
+        $this->view->formCode = highlight_file(
+            APPLICATION_PATH . '/forms/ColorPaletteExample.php', true
+        );
     }
 }
